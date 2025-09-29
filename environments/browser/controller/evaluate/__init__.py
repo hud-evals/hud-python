964ce32b8e88c855492c153c3f80290d96f1aeec
genteki <zhangkaiyuan.null@gmail.com>
2025-09-29 21:55:48 +0800

    Create autonomous_eval, delete vlm_judge

--- environments/browser/controller/evaluate/__init__.py
+++ environments/browser/controller/evaluate/__init__.py
@@ -3,12 +3,9 @@
 # Import modules to register @tool decorators
 from hud.tools.base import BaseHub
 
-evaluate = BaseHub("evaluate")
-
 from . import game_2048
 from . import todo
-from . import online_mind2web, webjudge_online_mind2web  # noqa: E402
+from . import autonomous_eval, webjudge_online_mind2web  # noqa: E402
 
 
-__all__ = ["game_2048", "todo", "online_mind2web", "webjudge_online_mind2web"]
-__all__.append("evaluate")
+__all__ = ["game_2048", "todo", "autonomous_eval", "webjudge_online_mind2web"]
